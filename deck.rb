e318edfbe117de7fcbd5a3717f8f40ac0f053208
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-08-28 09:55:58 -0400

    Split phrases.csv into three different decks
    Fixes #1

--- deck.rb
+++ deck.rb
@@ -1,9 +1,25 @@
 require 'squib'
 
-Squib::Deck.new(width: 750, height: 1023, cards: File.open('phrases.csv').count - 1, layout: 'phrases.yml') do
+Squib::Deck.new(width: 750, height: 1023, cards: File.open('start.csv').count - 1, layout: 'phrases.yml') do
   background color: '#D0E6FF'
-  phrases = csv file: 'phrases.csv'
+  phrases = csv file: 'start.csv'
   text range: :all, str: phrases['text'], layout: :EffectText
 
-  save_pdf file: 'phrases.pdf', gap: 5
+  save_pdf file: 'start.pdf', gap: 5
+end
+
+Squib::Deck.new(width: 750, height: 1023, cards: File.open('middle.csv').count - 1, layout: 'phrases.yml') do
+  background color: '#D0E6FF'
+  phrases = csv file: 'middle.csv'
+  text range: :all, str: phrases['text'], layout: :EffectText
+
+  save_pdf file: 'middle.pdf', gap: 5
+end
+
+Squib::Deck.new(width: 750, height: 1023, cards: File.open('end.csv').count - 1, layout: 'phrases.yml') do
+  background color: '#D0E6FF'
+  phrases = csv file: 'end.csv'
+  text range: :all, str: phrases['text'], layout: :EffectText
+
+  save_pdf file: 'end.pdf', gap: 5
 end
